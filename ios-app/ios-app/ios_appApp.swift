69afe97f1c6d465770064f801139513fd40adb66
aadikrishna04 <aadikrishna04@gmail.com>
2026-02-07 14:02:00 -0500

    skeleton

---
+++ ios-app/ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by Aadi Krishna on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
